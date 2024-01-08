99e9fc220ead5bcfa0ab67feea1e7a985d647eaf
José <123946943+JoseAQC@users.noreply.github.com>
2024-01-08 22:42:38 +0100

    Files from the FFT SDK project

---
+++ src_sdk/main.cc
@@ -0,0 +1,44 @@
+/*
+ * Empty C++ Application
+ */
+
+#include "Flexfft.h"
+#include "W_coef.h"
+#include "DataIn.h"
+#include "DataOut_OK.h"
+
+#define SAMPLES 512
+
+int main()
+{
+
+	sampleOutX_t outputData[FFT_LENGTH];
+	FlexFFT(DataIn, outputData);
+
+	/*
+	//medir el error de la aproximación
+    int i = 0;
+    float error = 0;
+    float errorRate = 0;
+    float realFlexFft = 0;
+    float realReference = 0;
+    float imagFlexFft = 0;
+    float imagReference = 0;
+
+   for(i=0; i<SAMPLES; i++)
+    {
+    	realFlexFft = real(outputData[i]);
+    	imagFlexFft = imag(outputData[i]);
+
+    	realReference = real(DataOut_OK[i]);
+    	imagReference= imag(DataOut_OK[i]);
+
+    	if((realFlexFft - realReference)> 0.1||(imagFlexFft - imagReference)> 0.1 )
+    		{
+    			error = 1;
+    		}
+    }
+    errorRate = error*100/SAMPLES;
+*/
+	return 0;
+}
